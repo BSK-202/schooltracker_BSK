379a825eaa7a4581eaa7f12a5a18cda09cd74ac6
Ikrame Baskane <baskaneikrame1@gmail.com>
2025-12-30 23:46:31 +0100

    url

--- front-end/src/config/baseUrl.js
+++ front-end/src/config/baseUrl.js
@@ -1,5 +1,6 @@
 const API_BASE_URL = __DEV__ 
-  ? 'http://192.168.0.135:3000' 
+  ? 'http://192.168.0.156:3000'
   : 'https://api.tondomaine.com';
-  
+  //192.168.0.156
+//192.168.0.135
 export default API_BASE_URL;
